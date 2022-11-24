bb6ec534aa16cfae01895a5e21ddbed164b79f60
Minh Thang <trantruongminhthang@gmail.com>
2022-11-24 14:54:24 +0700

    first commit

---
+++ src/main/java/com/example/Employee/services/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.example.Employee.services;
+
+import com.example.Employee.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    boolean deleteEmployee(long id);
+
+    Employee getEmployeeById(long id);
+
+    Employee updateEmployee(long id, Employee employee);
+}
